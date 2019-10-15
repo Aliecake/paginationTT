3671ce76fcaf01f3c0c309aa84df3e0f4c977a1b
Lauren Eggers <32617510+Aliecake@users.noreply.github.com>
2019-10-15 09:08:52 -0400

    refact createElements

--- js/script.js
+++ js/script.js
@@ -2,27 +2,13 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-
-
-showPage();
-
 /*
-   Reviewer: I realize rubric states this can be done with just TWO globals. I opted to use none. Nada.. Zip. Unless you consider searchResults.js searchedResults a global.
+   Reviewer: I realize rubric states this can be done with just TWO globals. I opted to use none in this file.
 */
 
-
-
-//helper function retrieves the HTMLcollection array-like-object and turns it into default array.
-function arrayGetter() {
-   const ul = document.querySelector('.student-list');
-   const arrayLi = [...ul.children];
-
-   return arrayLi;
-}
+showPage();
 
 function showPage() {
    const ul = document.querySelector('.student-list');
@@ -49,12 +35,10 @@ function pageSwap(start = 0, arrayLi = arrayGetter()) {
 }
 
 function appendPageLinks(arrayLi, studentList) {
-   const ul = document.createElement('ul');
+   //giving the ul class allows hover for all li
+   const ul = createElement('ul', 'className', 'pagination');
 
    insertAfter(ul, studentList);
-   //giving the ul the class active state works for all li
-   ul.className = 'pagination';
-
    createLi(arrayLi, ul);
 }
 
@@ -65,71 +49,25 @@ function createLi(arrayLi, ul) {
 
    for (let ii = 0; ii < numOfLinks; ii++){
       let pageNum = ii + 1;
-      const li = document.createElement('li');
-      
-      li.innerHTML = `<a href="#">${pageNum}</a>`;
+      const li = createElement('li', 'innerHTML', `<a href="#">${pageNum}</a>`);
       ul.append(li);
    }
 }
 
 function createSearch() {
    const h2 = document.querySelector('h2');
-   const form = document.createElement('form');
-   const input = document.createElement('input');
-   const button = document.createElement('button');
+   const form = createElement('form', 'className', 'student-search');
+   const input = createElement('input', 'type', 'text');
+   const button = createElement('button', 'innerText', 'Search');
 
    insertAfter(form, h2);
-   form.className = "student-search";
-   button.innerText = "Search";
+  
    form.appendChild(input);
    form.appendChild(button);
 
    input.setAttribute('placeholder', 'Search');
    input.setAttribute('id', 'search-input');
-   input.type = 'text';
 }
-
-document.addEventListener('click', (e) => {
-   if(e.target.nodeName === "A") {
-      //targets only pagination links prior to search
-      if(e.target.parentNode.parentNode.classList.contains('pagination') && !e.target.parentNode.parentNode.classList.contains('search-submitted')) {
-         //page number clicked to get starting index.
-         let start = (parseInt(e.target.innerText) - 1) * 10;
-         pageSwap(start);
-      } else {
-         //if click is after a search, use searched results
-         let start = (parseInt(e.target.innerText) - 1) * 10;
-         pageSwap(start, searchResults);
-      }
-   }
-});
-
-document.addEventListener('keyup', (e) => {
-   e.preventDefault();
-   
-   const input = document.getElementById('search-input');
-   const searchVal = input.value.toLowerCase();
-   const searched = [];
-   const arrayLi = arrayGetter();
-
-   arrayLi.forEach((li) => {
-      if(li.innerText.toLowerCase().indexOf(searchVal) === -1) {
-         li.style.display = 'none';
-      }
-      //search results
-      if(li.innerText.toLowerCase().indexOf(searchVal) !== -1) {
-         let p = document.querySelector('.no-results');
-         searched.push(li);
-         //removes message when user deletes search query
-         if (p) {
-            p.remove();
-         }
-      }
-   });
-   searchResults = searched;
-   pageSwap(0, searched);
-   paginationDisplay(searchResults.length);
-});
 
 function paginationDisplay(num) {
    const ul = document.querySelector('.pagination');
@@ -158,18 +96,77 @@ function paginationDisplay(num) {
 
 function noSearchResults() {
    let studentList = document.querySelector('.student-list');
-   let p = document.createElement('p');
+   let p = createElement('p', 'className', 'text-block');
    //wont post multiple p during keyup listener
    if (!document.querySelector('.no-results')) {
-      p.className = 'no-results';
+      p.classList.add('no-results');
       p.textContent = "No Search Results found, try again";
       insertAfter(p, studentList);
    }
 }
 
+///////// EVENT LISTENERS //////////
+
+document.addEventListener('click', (e) => {
+   if(e.target.nodeName === "A") {
+      //targets only pagination links prior to search
+      if(e.target.parentNode.parentNode.classList.contains('pagination') && !e.target.parentNode.parentNode.classList.contains('search-submitted')) {
+         //page number clicked to get starting index.
+         let start = (parseInt(e.target.innerText) - 1) * 10;
+         pageSwap(start);
+      } else {
+         //if click is after a search, use searched results
+         let start = (parseInt(e.target.innerText) - 1) * 10;
+         pageSwap(start, searchResults);
+      }
+   }
+});
+
+document.addEventListener('keyup', (e) => {
+   e.preventDefault();
+
+   const input = document.getElementById('search-input');
+   const searchVal = input.value.toLowerCase();
+   const searched = [];
+   const arrayLi = arrayGetter();
+
+   arrayLi.forEach((li) => {
+      if(li.innerText.toLowerCase().indexOf(searchVal) === -1) {
+         li.style.display = 'none';
+      }
+      //search results
+      if(li.innerText.toLowerCase().indexOf(searchVal) !== -1) {
+         let p = document.querySelector('.no-results');
+         searched.push(li);
+         //removes message when user deletes search query
+         if (p) {
+            p.remove();
+         }
+      }
+   });
+   searchResults = searched;
+   pageSwap(0, searched);
+   paginationDisplay(searchResults.length);
+});
+
+//////// HELPER FUNCTIONS /////////
+
+//helper function retrieves the HTMLcollection array-like-object and turns it into default array.
+function arrayGetter() {
+   const ul = document.querySelector('.student-list');
+   const arrayLi = [...ul.children];
+
+   return arrayLi;
+}
 //helper function to insertAfter, which isnt included in Vanilla
 //reference: https://plainjs.com/javascript/manipulation/insert-an-element-after-or-before-another-32/
 function insertAfter(el, refNode){
    refNode.parentNode.insertBefore(el, refNode.nextSibling);
 }
 
+//create helper
+function createElement(elemName, property, value) {
+   const element = document.createElement(elemName);
+   element[property] = value;
+   return element;
+}
